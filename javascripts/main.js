e6745b5c8cb7adde1b9d11bd77892f117abb7912
Arwen Hawes <ahawes@unca.edu>
2013-02-27 22:05:59 -0500

    add all javascripts including plugins

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,71 +1,42 @@
-
-
 (function () {
     "use strict";
+    /*global console: false, ctwitter: false, document: false, name: false, navigator: false, window: false */
     var $ = window.$,
+        main = function () {
+            //console.log("main called!"); Used to debug .click issues
+            //console.log($("#ui_button_1").size());
 
-    main = function () {
-        console.log("main called!");
-        console.log($("#ui_button_1").size())
-
-        var firstChoice = null,
-                secondChoice = null, //will store second choice 
-                contestantChoice = null,
-                morePopular = null, //will store which is more popular
-                firstCount = 0, //will count the first choice count from twitter
-                secondCount = 0, //will store the second choice count from twitter
-                winner = null; //set the contestants choice to win using jquery
-                var twitter = new ctwitter.CTwitter();
-
-        console.log("about to attach handler: " + $("#ui_button_1").size());
-        $("#ui_button_1").click(function() {
-            firstChoice = $("#user_input_1").val();
-            console.log("firstChoice updated to: " + firstChoice);
-            twitter.stream("statuses/filter", {lang: "en", track: [firstChoice]}, function(stream)
-        {
-            stream.on("data", function(tweet)
-            {
-                firstCount = firstCount + 1;
-                console.log(firstCount);
+            var userChoice = null,
+                count = 0, //will count the first choice count from twitter
+                twitter = new ctwitter.CTwitter();
+            $("#fittext_1").fitText();
+        //track choice and count it
+            //console.log("about to attach handler: " + $("#ui_button_1").size()); Used to debug .click
+            $("#ui_button").click(function () {
+                userChoice = $("#user_input").val();
+                $(".choices h1").hide();
+                $("#ui_button").hide();
+                $("#user_input").hide();
+                $(".choices").append("<h1> Choice:" + userChoice + "</h1>");
+                $("h1").css({"border-style": "solid", "border-width": "5px", "border-color": "white"});
+                console.log("firstChoice updated to: " + userChoice);
+                twitter.stream("statuses/filter", {lang: "en", track: [userChoice]}, function (stream) {
+                    stream.on("data", function (tweet) {
+                        count = count + 1;
+                        console.log(count);
+                    });
+                });
+            //console.log("handler attached!");
+                $(".choices").append("<h2> Count:" + count + "</h2>");
+                console.log("first choice: " + userChoice + " count: " + count);
+                if (count === 0) {
+                    $('.winner').avgrund({
+                        template: 'Winner!'
+                    });
+                }
             });
-        });
-        console.log("handler attached!");
-
-        //track first choice
-        
-        
-            
-        });
-        console.log("first choice: " + firstChoice + " count: " + firstCount);
-        //track second choice
-        $("#user_input_2").val("enter second choice");
-        $("#ui_button_2").click(function() {
-            secondChoice = $("#user_input_2").val();
-        });
-
-        var twitter2 = new ctwitter.CTwitter();
-        twitter2.stream("statuses/filter", {lang: "en", track: [secondChoice]}, function(stream)
-        {
-            stream.on("data", function(tweet)
-            {
-                secondCount = secondCount + 1;
-            });
-        });
-        console.log(secondChoice + secondCount);
-        //compare first and second tweet
-        if (firstCount > secondCount) {
-            winner = firstChoice;
-        }
-        else {
-            winner = secondChoice;
-        }
-        //compare contestantChoice & morePopular
-        if (winner === morePopular) {
-            //present winning choice
-
-        }
-
-    };
-    console.log("about to attach main to jquery");
+        //present winning choice
+        };
+    //console.log("about to attach main to jquery");
     $(document).ready(main); //abstracts main
 }());
